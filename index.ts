2d0a3e920f862d08001d0b41354e12ba776ab2d9
Kaleb Boothe <kboothe617@gmail.com>
2024-08-19 19:48:57 -0600

    menu loop and option list index.ts

--- index.ts
+++ index.ts
@@ -2,4 +2,30 @@ import inquirer from 'inquirer';
 
 const menu = async () => {
     let exit = false;
+
+    while (!exit) {
+        const { option } = await inquirer.prompt({
+            name: 'option',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments',
+                'Add Department',
+                'Delete Department',
+                'View Employees by Department',
+                'Update Employee Department',
+                'View All Roles',
+                'Add Role',
+                'Delete Role',
+                'Update Employee Role',
+                'View All Employees',
+                'Add Employee',
+                'Delete Employee',
+                'View Employee by Manager',
+                'Update Employee Manager',
+                'View Combined Salaries',
+                'Exit'
+            ]
+        })
+    }
 }
