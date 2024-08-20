c77f78777d4633ef1459f97f30c3434be2aa0e98
Kaleb Boothe <kboothe617@gmail.com>
2024-08-19 23:23:46 -0600

    employeeAdd export

--- queries.js
+++ queries.js
@@ -37,3 +37,8 @@ export const roleAdd = async (title, salary, department_id) => {
     const result = await pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *', [title, salary, department_id]);
     return result.rows[0];
 }
+
+export const employeeAdd = async (first_name, last_name, role_id, manager_id) => {
+    const result = await pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *', [first_name, last_name, role_id, manager_id]);
+    return result.rows[0];
+}
