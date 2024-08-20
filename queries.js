777d0e70d30b6b0a9d38b13c1e1769b0b2dab0e2
Kaleb Boothe <kboothe617@gmail.com>
2024-08-19 23:01:52 -0600

    getrole export

--- queries.js
+++ queries.js
@@ -4,3 +4,9 @@ export const getDepartments = async () => {
     const result = await pool.query('SELECT * FROM department');
     return result.rows;
 }
+
+export const getRoles = async () => {
+    const result = await pool.query('SELECT * role id, title, salary, department FROM role');
+    return result.rows;
+}
+
