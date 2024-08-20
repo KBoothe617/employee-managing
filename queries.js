6f8cda6733600b12ae9148d2ef926a63e5f91ea1
Kaleb Boothe <kboothe617@gmail.com>
2024-08-19 23:20:54 -0600

    departmentAdd export

--- queries.js
+++ queries.js
@@ -14,3 +14,22 @@ export const getEmployees = async () => {
     const result = await pool.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, \' \', manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id');
     return result.rows;
 }
+
+export const departmentAdd = async (name) => {
+    try {
+        const checkQuery = await pool.query('SELECT * FROM department WHERE name = $1', [name]);
+        const checkResult = await pool.query(checkQuery, [name]);
+
+        if (checkResult.rows.length > 0) {
+            console.log('Department already exists');
+            return null;
+        }
+
+        const result = await pool.query('INSERT INTO department (name) VALUES ($1) RETURNING *', [name]);
+        return result.rows[0];
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
